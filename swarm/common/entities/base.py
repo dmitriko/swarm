e98411f70ba1bc091417948eeebe703fd3913248
DmitriKo <dmitrikozhevin@gmail.com>
2012-10-06 22:05:56 +0800

    add Event abstract class

--- swarm/common/entities/base.py
+++ swarm/common/entities/base.py
@@ -2,6 +2,8 @@
 
 import time
 import uuid
+import json
+from copy import copy
 
 
 class Entity(object):
@@ -25,16 +27,3 @@ class Entity(object):
         info = copy(self.__dict__)
         info['cls'] = self.__class__.__name__
         return json.dumps(info)
-
-    @classmethod
-    def from_json(cls, json_str):
-        try:
-            info = dict((str(x), y) for x, y in json.loads(json_str).items())
-            cls_name = info['cls']
-        except (ValueError, KeyError): 
-            raise RuntimeError("JSON for enity is not valid %s %s" % json_str)
-        del info['cls']
-        entity_class = globals().get(cls_name)
-        if not entity_class:
-            raise RuntimeError("%s is not valid class name" % cls_name)
-        return entity_class(**info)
