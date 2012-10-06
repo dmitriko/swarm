1243e329d09952d2724969c9cc611d46c7f601e2
DmitriKo <dmitrikozhevin@gmail.com>
2012-10-06 22:08:18 +0800

    add base class from testing

---
+++ swarm/tests/base.py
@@ -0,0 +1,27 @@
+""" Base classes for UnitTest cases 
+The tests require U have local version
+of RabbitMQ up and running
+
+"""
+
+from tornado.testing import AsyncTestCase
+from tornado.options import parse_config_file, options
+
+from swarm.common.config import define_common_options
+import os
+
+DIR = os.path.realpath(os.path.dirname(__file__))
+
+class BaseTestCase(AsyncTestCase):
+    def setUp(self):
+        AsyncTestCase.setUp(self)
+        define_common_options()
+        parse_config_file(os.path.join(DIR, 'config_data.py'))
+
+    def tearDown(self):
+        for key in options.keys():
+            del options[key]
+
+        
+
+
