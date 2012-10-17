88bbdcfbd9993f85741b0009774703c9099108a0
DmitriKo <dmitrikozhevin@gmail.com>
2012-10-17 14:30:02 +0800

    implement Cluster

---
+++ swarm/cluster.py
@@ -0,0 +1,55 @@
+"Cluster instance represent state of the system"
+
+import threading
+from collections import defaultdict
+
+from swarm.entity import Entity
+
+
+class Cluster(object):
+
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        self._data = {}
+        self._entities = defaultdict(set)
+    
+    def get(self, oid, default=None):
+        return self._data.get(oid, default)
+
+    def ids_by_class(self, class_):
+        "Return list of stored ids for entites of given class"
+
+        if isinstance(class_, basestring):
+            class_name = class_
+        else:
+            assert issubclass(class_, Entity)
+            class_name = class_.__name__
+
+        return list(self._entities[class_name])
+
+    def entities_by_class(self, class_):
+        return [self._data.get(x) for x in self.ids_by_class(class_)]
+
+    def store(self, entity):
+        "Persist entity"
+        assert isinstance(entity, Entity)
+        self._entities[entity.__class__.__name__].add(entity.oid)
+        self._data[entity.oid] = entity
+
+    def delete(self, entity_or_oid):
+        "Remove item from storage, accept entity or oid"
+        if isinstance(entity_or_oid, Entity):
+            entity = entity_or_oid
+        else:
+            entity = self._data[entity_or_oid]
+        del self._data[entity.oid]
+        self._entities[entity.__class__.__name__].remove(entity.oid)
+            
+    @staticmethod
+    def instance():
+        if not hasattr(Cluster, "_instance"):
+            with Cluster._instance_lock:
+                if not hasattr(Cluster, "_instance"):
+                    Cluster._instance = Cluster()
+        return Cluster._instance
---
+++ swarm/tests/test_cluster.py
@@ -0,0 +1,33 @@
+from .base import BaseTestCase
+from swarm.cluster import Cluster
+from swarm.entity import Entity
+
+class Item(Entity):
+    def __init__(self, name, **kw):
+        self.name = name
+        Entity.__init__(self, **kw)
+
+class Baz(Item): pass
+    
+class ClusterCase(BaseTestCase):
+
+    def test_instance(self):
+        self.assertEqual(
+            Cluster.instance(), Cluster.instance())
+
+    def test_entites_storing(self):
+        cluster = Cluster.instance()
+        item1 = Item('foo')
+        item2 = Item('bar')
+        item3 = Baz('baz')
+        cluster.store(item1)
+        cluster.store(item2)
+        cluster.store(item3)
+        
+        self.assertEqual(cluster.get(item1.oid), item1)
+        self.assertEqual(len(cluster.entities_by_class('Item')), 2)
+        self.assertEqual(len(cluster.entities_by_class(Baz)), 1)
+        self.assertEqual(cluster.entities_by_class(Baz)[0],
+                         item3)
+        cluster.delete(item1)
+        self.assertEqual(len(cluster.ids_by_class(Item)), 1)
