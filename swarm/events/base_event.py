a7e3bee8c6c564ba76fac429708aad8044ec1cc4
DmitriKo <dmitrikozhevin@gmail.com>
2012-10-18 23:03:08 +0800

    implement Event.node_oid

--- swarm/events/base_event.py
+++ swarm/events/base_event.py
@@ -7,5 +7,6 @@ class Event(Entity):
 
     def __init__(self, reporter, **kw):
         self.reporter = reporter
+        self.node_oid = kw.get('node_oid', reporter)
         Entity.__init__(self, **kw)
     
